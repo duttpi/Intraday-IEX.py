52a16c3cc4d094b910c1214d97179cfd14bdb469
Pikaso Dutta <40728525+duttpi@users.noreply.github.com>
2018-09-24 17:34:04 -0400

    Update IEX.py

--- IEX.py
+++ IEX.py
@@ -1,14 +1,23 @@
 
-import time
 import pandas as pd
- 
-Ticker = 'MSFT'
-Today  = int(time.strftime("%Y%m%d"))
-Url = 'https://api.iextrading.com/1.0/stock/' + Ticker +'/chart/date/' + str(Today)
+from pandas.tseries.offsets import BDay
 
-Data = pd.DataFrame(pd.read_json(Url))
-Data = Data.dropna()
-Data['date'] = Data['date'].map(str) + '-' + Data['label']
-Data.set_index('date', inplace=True)
-Data = Data[['open','high','low','close','volume']]
+def IEX(Days, Ticker):
+    Date = []
+    today = pd.datetime.today()
+    for n in range (0, Days):
+        x = (today - BDay(n)).strftime('%Y%m%d')
+        Date.append(x)
 
+    global Data
+    Data = pd.DataFrame()
+    for n in range (0, Days):
+        Url = 'https://api.iextrading.com/1.0/stock/'+Ticker+'/chart/date/'+str(Date[n])
+        Df = pd.DataFrame(pd.read_json(Url))
+        Df = Df.dropna()
+        Df['date'] = Df['date'].map(str) + '-' + Df['label']
+        Df.set_index('date', inplace=True)
+        Df.index = pd.to_datetime(Df.index)
+        Df = Df[['open','high','low','close','volume']]
+        Df.columns = ['Open','High','Low','Close','Volume']
+        Data = Data.append(Df, ignore_index=False)
